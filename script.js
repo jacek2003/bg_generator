398d4ee383a3872a1817a1b4e600790935129391
Jacek <jacekkuznicki2@gmail.com>
2023-02-08 16:48:38 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,38 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.querySelector(".btn");
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+function getRandomGradient(){
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    setGradient();
+}
+
+setGradient();
+
+function setGradient () {
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+btn.addEventListener("click", getRandomGradient);
+
